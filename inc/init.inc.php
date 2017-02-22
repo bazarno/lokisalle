f22e2579c736f7e7f1a6ea6e71d1cc70bc314025
arno <arnotrafic@gmail.com>
2017-02-22 15:32:08 +0100

    22/02
    15h30

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,6 +1,6 @@
 <?php
 // Connexio BDD
-$pdo = new PDO('mysql:host=localhost;dbname=site', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=localhost;dbname=lokisalle', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 // Session
 session_start();
